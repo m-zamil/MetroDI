9c1f631c5e59f304a2d7613aa513bba7561a6b5e
Zamil <muzamil-javascriptdev@outlook.com>
2024-03-27 08:17:21 +0500

    About us page completed along with minor adjustments on homepage

---
+++ assets/js/about.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.history-slider', {
+  a11y: false,
+  spaceBetween: 20,
+  breakpoints: {
+    1280: {
+      spaceBetween: 30,
+    },
+    //   1600: {
+    //     spaceBetween: 50,
+    //   },
+  },
+  freeMode: true,
+  slidesPerView: 'auto',
+});
